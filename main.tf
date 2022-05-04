c3d5b5dc64c8086e1c1aa1938537a4d6193e8b25
Mathias Becher <mbecher@thinkport.digital>
2022-05-04 10:45:08 +0200

    Fix CIDR notation

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "azurerm_subnet" "subnet" {
   name                 = "snet-${var.project}-${var.environment}"
   resource_group_name  = azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.this.name
-  address_prefixes     = ["10.0.0.1/24"]
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_network_interface" "nic" {
